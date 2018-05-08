2204b9946775c947ed0f5385100e12ce616b3993
cc_surfacePro3 <carbajal.carlos@hotmail.com>
2018-05-08 12:53:30 +0100

    Installation of required packages and initial functions

--- text_analysis/1_text_analysis.R
+++ text_analysis/1_text_analysis.R
@@ -1,1 +1,19 @@
+# set a working folder within the tutorials
 setwd("D:/code/r/tutorials/text_analysis")
+
+# Installing required packages
+install.packages("quanteda", dependencies = TRUE)
+install.packages("readtext")
+install.packages("devtools")
+devtools::install_github("quanteda/quanteda.corpora")
+
+# Loading required packages
+require(quanteda)
+require(readtext)
+
+data_dir <- system.file("extdata/", package = "readtext")
+inaug_data <- readtext(paste0(data_dir, "/csv/inaugCorpus.csv"), text_field="texts")
+View(inaug_data)
+
+udhr_data <- readtext(paste0(data_dir, "/txt/UDHR/*"))
+fix(udhr_data)
